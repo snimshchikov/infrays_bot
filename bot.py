fc5d99dfb78bc3241e8c19b962d1028f3d2e3f1d
IlyaVkBot <36227250+IlyaVkBot@users.noreply.github.com>
2021-12-26 19:02:08 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+from telegram.ext import Updater
+from secret.py import token
+updater = Updater(token=token, use_context=True)
+
+dispatcher = updater.dispatcher
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+from telegram import Update
+from telegram.ext import CallbackContext
+
+
+from telegram.ext import MessageHandler, Filters
+def chat_sender_handler(update: Update, context: CallbackContext):
+    message = update.message
+    if message.sender_chat:
+        context.bot.ban_chat_sender_chat(chat_id=update.effective_chat.id, sender_chat_id=message.sender_chat.id)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Забанил новый канал Имя: {message.sender_chat.title} - Тег: {message.sender_chat.username}!")
+dispatcher.add_handler(MessageHandler(Filters.update, chat_sender_handler))
+
+
+updater.start_polling()
